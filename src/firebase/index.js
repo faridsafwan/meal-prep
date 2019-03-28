4ff384051fc749928ae937ed84756e56fd80b7bd
Muhamad Farid Safwan <faridsafwan@outlook.com>
2019-03-28 10:23:08 +0800

    first commit

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD3KErldHGTSCsD5l9KaXYzGoBFJzpfmmo",
+  authDomain: "meal-prep-dc038.firebaseapp.com",
+  databaseURL: "https://meal-prep-dc038.firebaseio.com",
+  projectId: "meal-prep-dc038",
+  storageBucket: "meal-prep-dc038.appspot.com",
+  messagingSenderId: "827502672040"
+};
+firebase.initializeApp(config);
